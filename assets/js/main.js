3c4978cd3a260cbb9ab53016ea6f633f3a4213b1
kradhenz <tomas25498@gmail.com>
2024-02-14 13:37:25 -0300

    push, shift and pop requirement added

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,10 +27,28 @@ var dental = [
     {hora: '14:00', especialista: 'RAQUEL VILLASECA',       paciente: 'ANA SEPULVEDA',  rut: '14441281-0', prevision: 'ISAPRE'},
 ];
 
-//Agregar código para el desafio 2 aquí
+/*      START        */
+// 1st Requeriment
+traumatologia.push(
+    {
+        hora: "09:00", especialista: "RENÉ POBLETE", paciente: "ANA GELLONA", rut: "13123329-7", prevision: "ISAPRE"
+    },{
+        hora: "09:30", especialista: "MARIA SOLAR", paciente: "RAMIRO ANDRADE", rut: "12221451-K", prevision: "FONASA"
+    },{
+        hora: "10:00", especialista: "RAUL LOYOLA", paciente: "CARMEN ISLA", rut: "10112348-3", prevision: "ISAPRE"
+    },{
+        hora: "10:30", especialista: "ANTONIO LARENAS", paciente: "PABLO LOAYZA", rut: "13453234-1", prevision: "ISAPRE"
+    },{
+        hora: "12:00", especialista: "MATIAS ARAVENA", paciente: "SUSANA POBLETE", rut: "14345656-6", prevision: "FONASA"
+    }
+);
+console.log(traumatologia);
+// 2nd Requeriment
+radiologia.shift();
+radiologia.pop();
+console.log(radiologia);
 
-
-
+// 3rd Requirement
 
 //Agregar código para el desafio 2 aquí
 
